0bb5cdc241b5b56740d76617b8409d29c7b10b2c
Krithika Kondapalli <krithika.kondapalli@gmail.com>
2025-09-26 20:32:05 -0400

    part 4 add tests to reach full code coverage

--- grade-calculator/grade_calculator_test.go
+++ grade-calculator/grade_calculator_test.go
@@ -1,6 +1,9 @@
 package esepunittests
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestGetGradeA(t *testing.T) {
 	expected_value := "A"
@@ -34,6 +37,38 @@ func TestGetGradeB(t *testing.T) {
 	}
 }
 
+func TestGetGradeC(t *testing.T) {
+	expected_value := "C"
+
+	gradeCalculator := NewGradeCalculator()
+
+	gradeCalculator.AddGrade("open source assignment", 70, Assignment)
+	gradeCalculator.AddGrade("exam 1", 70, Exam)
+	gradeCalculator.AddGrade("essay on ai ethics", 70, Essay)
+
+	actual_value := gradeCalculator.GetFinalGrade()
+
+	if expected_value != actual_value {
+		t.Errorf("Expected GetGrade to return '%s'; got '%s' instead", expected_value, actual_value)
+	}
+}
+
+func TestGetGradeD(t *testing.T) {
+	expected_value := "D"
+
+	gradeCalculator := NewGradeCalculator()
+
+	gradeCalculator.AddGrade("open source assignment", 60, Assignment)
+	gradeCalculator.AddGrade("exam 1", 65, Exam)
+	gradeCalculator.AddGrade("essay on ai ethics", 60, Essay)
+
+	actual_value := gradeCalculator.GetFinalGrade()
+
+	if expected_value != actual_value {
+		t.Errorf("Expected GetGrade to return '%s'; got '%s' instead", expected_value, actual_value)
+	}
+}
+
 func TestGetGradeF(t *testing.T) {
 	expected_value := "F"
 
@@ -41,6 +76,26 @@ func TestGetGradeF(t *testing.T) {
 
 	gradeCalculator.AddGrade("open source assignment", 45, Assignment)
 	gradeCalculator.AddGrade("exam 1", 35, Exam)
+	gradeCalculator.AddGrade("essay on ai ethics", 50, Essay)
+
+	actual_value := gradeCalculator.GetFinalGrade()
+
+	if expected_value != actual_value {
+		t.Errorf("Expected GetGrade to return '%s'; got '%s' instead", expected_value, actual_value)
+	}
+}
+
+func TestGetGradeEmptyList(t *testing.T) {
+	expected_value := "F"
+
+	gradeCalculator := NewGradeCalculator()
+
+	gradeCalculator.AddGrade("open source assignment", 0, Assignment)
+	// No Exam Score - Assume a student missed an exam and did not make it up.
+	// gradeCalculator.AddGrade("exam 1", 35, Exam)
+
+	// I added this statement to get 100% code coverage
+	fmt.Println(Assignment.String())
 	gradeCalculator.AddGrade("essay on ai ethics", 50, Essay)
 
 	actual_value := gradeCalculator.GetFinalGrade()
